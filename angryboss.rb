f59a398b64b55ff5445593e2ad58937bde9731b4
abosarge <abosarge@gmail.com>
2015-06-24 19:17:17 -0400

    20150625 Added angryboss file

---
+++ angryboss.rb
@@ -0,0 +1,3 @@
+puts "What the hell do you want?".upcase
+sadsack = gets.chomp
+puts ("What the eff do you mean " + sadsack + "? That's the dumbest thing I've ever heard! You're fired dumbass!").upcase
