80678cbe730b25d58a1f37ecf0a1993aa3e77668
abosarge <abosarge@gmail.com>
2015-06-24 20:32:16 -0400

    Added loop, iceream, and names.

---
+++ icecream.rb
@@ -0,0 +1,11 @@
+puts "What is your favorite flavor of ice cream?"
+
+icecream = gets.chomp
+
+if icecream == "chocolate" 
+	puts "Awesome! Great choice!"
+elsif icecream == "vanilla" 
+	puts "You're a moron with no tastebuds."	
+else
+	puts "I only eat chocolate flavored desserts."
+end
---
+++ names.rb
@@ -0,0 +1,10 @@
+puts "Hello, what's your name?"
+name = gets.chomp
+
+puts "Hello, " + name + "."
+
+if name == "Amanda"
+    puts "That is the best name!"
+elsif name == "Michelle"
+    puts "That's a good name, too!"
+end
